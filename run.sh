dbcb238d1179786bae60e3028c77c58a6f7ff3a8
bifferos <bifferos@yahoo.co.uk>
2011-09-11 02:12:07 +0100

    Stop e1000 driver from complaining about missing boot ROM

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@
 ./qemu/i386-softmmu/qemu -bios /tmp/bios.bin  \
 	-firmware openwrt/bin/rdc/openwrt-rdc-jffs2-64k-bifferboard.img   \
 	-kernel openwrt/bin/rdc/openwrt-rdc.bzImage   \
+	-append rootfstype=jffs2   \
 	-vga none -nographic \
 	-L qemu/pc-bios/optionrom
 
